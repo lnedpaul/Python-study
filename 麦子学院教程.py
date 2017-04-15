388b906b058fb536c1ef5b6f23436443daf0d92e
lnedpaul <paul82.zhu@gmail.com>
2017-04-15 21:09:49 +0800

    v1

--- 麦子学院教程.py
+++ 麦子学院教程.py
@@ -3,9 +3,28 @@
 import os
 print(os.getcwd())
 
-
 #抓取网站对象附给r
 import requests
 r = requests.get("http://www.youku.com")
 print(r.url)
 print(r.encoding)
+
+#数据类型
+import sys
+a = 3
+b = 4
+
+c = 5.66
+d = 8.0
+
+e = complex(c,d)
+f = complex(float(a), float(b))
+
+print("a is type: ", type(a))
+print("c is type: ", type(c))
+print("e is type: ", type(e))
+
+print(a + b)
+print(d / c)
+print(e + f)
+print(sys.float_info)
