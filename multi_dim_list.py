a2e53a8ef2b33bd44c3235b8a2b994b9e61ef6a2
lnedpaul <paul82.zhu@gmail.com>
2017-04-27 23:35:52 +0800

    code

--- multi_dim_list.py
+++ multi_dim_list.py
@@ -1,3 +1,12 @@
-a = [1,2,3,4,5]
-multi_dim_list = [[1,2,3],[3,4,5],[5,6,7]]
-print(multi_dim_list[1][2])
+ori_lst = list(range(1,11))
+
+def movebackward():
+    ori_lst.append(ori_lst.pop(0))
+    return ori_lst
+
+def application_times(x):
+    for i in x:
+        movebackward()
+
+
+print (application_times(3))
